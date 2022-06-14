24a7c67335f8c36792d31e6a7e9845cf837ea9f3
Ajay C gaint <100661470+Ajay18C@users.noreply.github.com>
2022-06-14 14:42:22 +0000

    boost luck

--- main.swift
+++ main.swift
@@ -1,10 +1,10 @@
-/*class Secret{
+class Secret{
   func display(){}
   func LuckMeter(){}
 }
 class sumOfDigits:Secret{
   private var num:Int;
-  private var res:Int;
+  var res:Int;
   init(num:Int){
     self.num=num
     self.res=0
@@ -23,22 +23,22 @@ class sumOfDigits:Secret{
     switch(self.res/3){
       case 0:
       print("::::::::::::::::::::::::::::::::::::::::")
-      print("-----100% LUCK🍀-----")
+      print("-----100% LUCK 🍀-----")
       print("                                        ")
       print("All Belongs To You")
       case 1:
       print("::::::::::::::::::::::::::::::::::::::::")
-      print("-----75% LUCK☘️-----")
+      print("-----75% LUCK ☘️-----")
       print("                                        ")
       print("The only thing that overcomes hard luck is hard work.")
       case 2:
       print("::::::::::::::::::::::::::::::::::::::::")
-      print("-----50% luck🌱-----")
+      print("-----50% LUCK 🌱-----")
       print("                                        ")
       print("Concentration attracts luck factor.")
       case 3:
       print("::::::::::::::::::::::::::::::::::::::::")
-      print("-----30% LUCK🍁-----")
+      print("-----30% LUCK 🍁-----")
       print("                                        ")
       print("Luck is believing you’re lucky.")
       default:
@@ -51,7 +51,7 @@ class FortuneTeller{
   private var age:Int;
   private var color:String;
   var sum=0;
-  var luckyNum=0;
+  var luckyNumber=0
   init(name:String,age:Int,color:String){
     self.name=name;
     self.age=age;
@@ -70,6 +70,7 @@ class FortuneTeller{
     let SOD=sumOfDigits(num:sum)//creating object for sumOfDigits class as SOD
     SOD.display()
     SOD.LuckMeter()
+    self.luckyNumber=SOD.res/3
   }
 }
 //____main function____
@@ -96,22 +97,28 @@ print("*****ENTER YOUR FAVORITE COLOR ***** :");
 let color=readLine();//get fav color from user
 let obj1=FortuneTeller(name:name!,age:age!,color:color!);//creating object for FortuneTeller class as obj1
 obj1.CalculateFortune()//calling class method using object
-print("                                        ")
-print("-----PRESS ENTER TO RETRY-----")
-let anmi=readLine()
+if(obj1.luckyNumber<3){
+  print("\n-----PRESS 1 TO BOOST YOUR LUCK-----")
+  let anmi=readLine()
 if(anmi != ""){
   break
 }
 }
-*/
-print("Enter 1 to check your Luck \n\nEnter 2 to boost your luck")
-let choice=Int(readLine()!) 
+  else{
+print("\n-----PRESS ENTER TO RETRY-----")
+let anmi=readLine()
+if(anmi != ""){
+  break
+}
+  }
+}
+/*let choice=Int(readLine()!) 
 var luck :Int
 switch choice {
   case 2:
   print("\n\n::::::::::::::::::::::::::::::::::::::::\n");
   print("-----THIS IS FORTUNE BOOSTER-----");
-  print();
+  print("Here you can boost your luck");
   default:
     break
-}
+}*/
